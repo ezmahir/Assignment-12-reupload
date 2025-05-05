f02a59c55acf05ef4cd5887551e535057ab7e25c
ProgrammingHero1 <mahirruphy@gmail.com>
2025-05-05 14:38:31 +0600

    reviewed page

--- src/Components/TrendingProducts/TrendingProducts.jsx
+++ src/Components/TrendingProducts/TrendingProducts.jsx
@@ -14,7 +14,7 @@ const FeaturedProducts = () => {
     <div className="w-full rounded-3xl p-7 my-10 shadow-2xl">
       <div className="text-center">
         <p className="text-5xl font-bold mb-16">
-          Latest 4 Trending Products based on Upvotes
+          Latest 4 Trending Products Based on Upvotes
         </p>
       </div>
       <div className="grid grid-cols-2 space-y-10 space-x-10">
