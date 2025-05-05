80e2f6cbfe0d2851cccb81610a2276742db518e7
ProgrammingHero1 <mahirruphy@gmail.com>
2025-05-05 14:42:25 +0600

    Errorpage redesign

--- src/Components/ErrorPage/ErrorPage.jsx
+++ src/Components/ErrorPage/ErrorPage.jsx
@@ -3,10 +3,10 @@ const Errorpage = () => {
   return (
     <div>
       <div className="text-center my-16 space-y-10">
-        <h1 className="font-black text-7xl">OOPS...</h1>
+        <h1 className="font-black text-7xl">OOPSIE...</h1>
         <p className="font-light text-5xl">Status Code: 404</p>
         <p className="font-extralight text-xl">
-          The Page you are looking for is not available right now
+          The Page you are looking for is not available right now, Sorry!
         </p>
         <img className="mx-auto rounded-full h-96" src={ErrorImg} alt="" />
       </div>
